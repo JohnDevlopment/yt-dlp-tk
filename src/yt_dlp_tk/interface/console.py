c050df413f9b3bce390c6e76ef8bba2b65feb2a1
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2023-04-23 20:57:26 -0500

    Interface: Make ConsoleWindow a single-instance class
    When "closed", it actually just hides itself.

--- src/yt_dlp_tk/interface/console.py
+++ src/yt_dlp_tk/interface/console.py
@@ -4,19 +4,43 @@ from __future__ import annotations
 from . import ExText
 from .utils import InState
 from ..logging import get_logger, add_handler
+from ..signals import signal
 from tkinter import ttk, Misc, constants as tkconst, Toplevel
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from typing import Any
+    from typing import Any, Type
+    from typing_extensions import Self
 
 class ConsoleWindow(Toplevel):
     """Console window."""
+
+    _instance: ConsoleWindow | None = None
+
+    LINE_LIMIT = 200
+
+    @classmethod
+    def show(cls: Type[Self]) -> Self:
+        """
+        Show the console window.
+
+        Creates the window on first invocation, then
+        returns it on successive calls.
+        """
+        if cls._instance is None:
+            cls._instance = cls()
+            return cls._instance
+
+        cls._instance.deiconify()
+        return cls._instance
 
     def __init__(self, master: Misc | None=None,
                  cnf: dict[str, Any]={}, **kw: Any):
         super().__init__(master, cnf, **kw)
         self._body()
+        self._mapped = True
+        self.on_show = signal('show', self)
+        self.on_close = signal('close', self)
 
     def _body(self):
         self.frame = ttk.Frame(self)
@@ -25,11 +49,17 @@ class ConsoleWindow(Toplevel):
         self.text = ExText(self.frame, state='disabled')
         self.text.pack(fill=tkconst.BOTH)
 
+        ttk.Button(self.frame, text="Close", command=self.close).pack()
+
     def write(self, text: str, /):
         with InState(self.text, 'normal'):
             self.text.insert(tkconst.END, text)
 
-    def flush(self): ...
+    def flush(self):
+        pass
+
+    def close(self):
+        self.wm_withdraw()
 
 def test ():
     from tkinter import ttk
@@ -43,10 +73,8 @@ def test ():
     frame.pack(fill=tk.BOTH, expand=True)
 
     def _btn1_cmd(btn: ttk.Button):
-        btn.after_idle(btn.destroy)
-
         logger = get_logger('tests.gui.console', stream=False)
-        console = ConsoleWindow()
+        console = ConsoleWindow.show()
         add_handler(logger, 'stream', stream=console)
 
         def log():
@@ -58,9 +86,9 @@ def test ():
 
         console.after(1000, log)
 
-    btn = ttk.Button(frame, text='Show Console')
-    btn.config(command=lambda: _btn1_cmd(btn))
-    btn.pack()
+    button = ttk.Button(frame, text='Show Console')
+    button.config(command=lambda: _btn1_cmd(button))
+    button.pack()
 
     root.mainloop()
 
