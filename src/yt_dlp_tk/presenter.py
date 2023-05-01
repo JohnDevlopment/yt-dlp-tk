3305fd689b907ab44740703fa8ce9a18e53767b1
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2023-04-30 20:55:45 -0500

    Presenter: Add logging messages

--- src/yt_dlp_tk/presenter.py
+++ src/yt_dlp_tk/presenter.py
@@ -9,16 +9,27 @@ if TYPE_CHECKING:
 
 class Presenter:
     def __init__(self, model: Model, view: View) -> None:
+        logger = get_logger('backend')
+
+        logger.info("Initializing backend...")
+
         self.model = model
+        logger.debug("Model initialized.")
         self.view = view
         self.view.create_interface(self)
+        logger.debug("Interface created.")
 
     def run(self) -> None:
         self.view.mainloop()
 
     def get_video_info(self) -> None:
+        logger = get_logger('backend.youtube')
+
+        logger.info("Retrieving info for %s.", self.view.url)
+
         result = self.model.get_video_info(self.view.url)
         if result.ok is not None:
+            logger.info("Retrieve info for video.")
             info = result.ok
             self.view.update_video_info(info)
 
